4c04184b2ae98ca222eb2513c07206bcd0026627
fortunechidiebere168-jpg <fortunechidiebere168@gmail.com>
2026-01-22 13:32:46 -0800

    Create book.py

---
+++ book.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self, book_id, title, author):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.is_issued = False
+
+    def mark_issued(self):
+        self.is_issued = True
+
+    def mark_returned(self):
+        self.is_issued = False
+
+    def __str__(self):
+        status = "Issued" if self.is_issued else "Available"
+        return f"[{self.book_id}] {self.title} by {self.author} - {status}"
